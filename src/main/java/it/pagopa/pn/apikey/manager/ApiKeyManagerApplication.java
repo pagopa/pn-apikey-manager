09d8a3eb4962b6b35d1df5a3af9def52e06e1f11
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2022-10-14 12:07:59 +0200

    start branch main

---
+++ src/main/java/it/pagopa/pn/apikey/manager/ApiKeyManagerApplication.java
@@ -0,0 +1,13 @@
+package it.pagopa.pn.apikey.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKeyManagerApplication.class, args);
+    }
+
+}
