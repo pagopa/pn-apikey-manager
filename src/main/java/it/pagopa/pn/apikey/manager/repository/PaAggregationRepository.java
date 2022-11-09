705a0ea536dcd51a2933bf53784ed5114ac8b49c
Simone Martelli <simone.martelli@emea.nttdata.com>
2022-11-09 09:44:39 +0100

    batch save PaAggregation

--- src/main/java/it/pagopa/pn/apikey/manager/repository/PaAggregationRepository.java
+++ src/main/java/it/pagopa/pn/apikey/manager/repository/PaAggregationRepository.java
@@ -1,14 +1,20 @@
 package it.pagopa.pn.apikey.manager.repository;
 
 import it.pagopa.pn.apikey.manager.entity.PaAggregationModel;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import software.amazon.awssdk.enhanced.dynamodb.model.BatchWriteResult;
 import software.amazon.awssdk.enhanced.dynamodb.model.Page;
+
+import java.util.List;
 
 public interface PaAggregationRepository {
 
     Mono<PaAggregationModel> searchAggregation(String xPagopaPnCxId);
 
     Mono<PaAggregationModel> savePaAggregation(PaAggregationModel toSave);
+
+    Flux<BatchWriteResult> savePaAggregation(List<PaAggregationModel> toSave);
 
     Mono<Page<PaAggregationModel>> findByAggregateId(String aggregateId, Integer limit, String lastKey);
 
--- src/main/java/it/pagopa/pn/apikey/manager/repository/PaAggregationRepositoryImpl.java
+++ src/main/java/it/pagopa/pn/apikey/manager/repository/PaAggregationRepositoryImpl.java
@@ -4,16 +4,16 @@ import it.pagopa.pn.apikey.manager.entity.PaAggregationModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.enhanced.dynamodb.model.Page;
-import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
-import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
+import software.amazon.awssdk.enhanced.dynamodb.model.*;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -21,12 +21,14 @@ import java.util.Map;
 public class PaAggregationRepositoryImpl implements PaAggregationRepository {
 
     private final DynamoDbAsyncTable<PaAggregationModel> table;
+    private final DynamoDbEnhancedAsyncClient dynamoDbEnhancedClient;
     private final String gsiAggregateId;
 
     public PaAggregationRepositoryImpl(DynamoDbEnhancedAsyncClient dynamoDbEnhancedClient,
                                        @Value("${pn.apikey.manager.dynamodb.pa-aggregations.gsi-name.aggregate-id}") String gsiAggregateId,
                                        @Value("${pn.apikey.manager.dynamodb.tablename.pa-aggregations}") String tableName) {
         this.table = dynamoDbEnhancedClient.table(tableName, TableSchema.fromBean(PaAggregationModel.class));
+        this.dynamoDbEnhancedClient = dynamoDbEnhancedClient;
         this.gsiAggregateId = gsiAggregateId;
     }
 
@@ -42,6 +44,23 @@ public class PaAggregationRepositoryImpl implements PaAggregationRepository {
     @Override
     public Mono<PaAggregationModel> savePaAggregation(PaAggregationModel toSave) {
         return Mono.fromFuture(table.putItem(toSave)).thenReturn(toSave);
+    }
+
+    @Override
+    public Flux<BatchWriteResult> savePaAggregation(List<PaAggregationModel> toSave) {
+        return Flux.fromIterable(toSave)
+                .window(25)
+                .flatMap(chunk -> {
+                    WriteBatch.Builder<PaAggregationModel> builder = WriteBatch.builder(PaAggregationModel.class)
+                            .mappedTableResource(table);
+                    Mono<BatchWriteResult> deferred = Mono.defer(() ->
+                            Mono.fromFuture(dynamoDbEnhancedClient.batchWriteItem(BatchWriteItemEnhancedRequest.builder()
+                                    .writeBatches(builder.build())
+                                    .build())));
+                    return chunk
+                            .doOnNext(builder::addPutItem)
+                            .then(deferred);
+                });
     }
 
     @Override
