8f8658a133836bd489ae6b6a1e8f556b661a1075
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2022-11-10 12:19:47 +0100

    - add external-registries integration for list of onboarding PA

---
+++ src/main/java/it/pagopa/pn/apikey/manager/client/ExternalRegistriesClient.java
@@ -0,0 +1,42 @@
+package it.pagopa.pn.apikey.manager.client;
+
+import it.pagopa.pn.apikey.manager.exception.ApiKeyManagerException;
+import it.pagopa.pn.apikey.manager.generated.openapi.rest.v1.aggregate.dto.PaDetailDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class ExternalRegistriesClient {
+
+    private final WebClient webClient;
+
+    protected ExternalRegistriesClient(ExternalRegistriesWebClient externalRegistriesWebClient) {
+        this.webClient = externalRegistriesWebClient.init();
+    }
+
+    public Mono<List<PaDetailDto>> callExternalRegistries(String name) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParamIfPresent("paNameFilter", Optional.of(name))
+                        .path("/ext-registry/pa/v1/activated-on-pn")
+                        .build())
+                .headers(httpHeaders -> httpHeaders.setContentType(MediaType.APPLICATION_JSON))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<PaDetailDto>>() {})
+                .doOnError(throwable -> {
+                    if (throwable instanceof WebClientResponseException) {
+                        WebClientResponseException ex = (WebClientResponseException) throwable;
+                        throw new ApiKeyManagerException(ex.getMessage(), ex.getStatusCode());
+                    }
+                });
+    }
+}
---
+++ src/main/java/it/pagopa/pn/apikey/manager/client/ExternalRegistriesWebClient.java
@@ -0,0 +1,45 @@
+package it.pagopa.pn.apikey.manager.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
+
+@Component
+@Slf4j
+public class ExternalRegistriesWebClient extends CommonWebClient {
+
+    private final Integer tcpMaxPoolSize;
+    private final Integer tcpMaxQueuedConnections;
+    private final Integer tcpPendingAcquireTimeout;
+    private final Integer tcpPoolIdleTimeout;
+    private final String basePath;
+
+    public ExternalRegistriesWebClient(@Value("${pn.apikey.manager.webclient.pn-external-registries.tcp-max-poolsize}") Integer tcpMaxPoolSize,
+                                       @Value("${pn.apikey.manager.webclient.pn-external-registries.tcp-max-queued-connections}") Integer tcpMaxQueuedConnections,
+                                       @Value("${pn.apikey.manager.webclient.pn-external-registries.tcp-pending-acquired-timeout}") Integer tcpPendingAcquireTimeout,
+                                       @Value("${pn.apikey.manager.webclient.pn-external-registries.tcp-pool-idle-timeout}")Integer tcpPoolIdleTimeout,
+                                       @Value("${pn.apikey.manager.pn-external-registries.base-path}")String basePath) {
+        this.tcpMaxPoolSize = tcpMaxPoolSize;
+        this.tcpMaxQueuedConnections = tcpMaxQueuedConnections;
+        this.tcpPendingAcquireTimeout = tcpPendingAcquireTimeout;
+        this.tcpPoolIdleTimeout = tcpPoolIdleTimeout;
+        this.basePath = basePath;
+    }
+
+    public WebClient init() {
+        ConnectionProvider provider = ConnectionProvider.builder("fixed")
+                .maxConnections(tcpMaxPoolSize)
+                .pendingAcquireMaxCount(tcpMaxQueuedConnections)
+                .pendingAcquireTimeout(Duration.ofMillis(tcpPendingAcquireTimeout))
+                .maxIdleTime(Duration.ofMillis(tcpPoolIdleTimeout)).build();
+
+        HttpClient httpClient = HttpClient.create(provider);
+
+        return super.initWebClient(httpClient,basePath);
+    }
+}
