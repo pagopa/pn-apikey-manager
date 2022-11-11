04ebf2f0634d61e2e349ef866595a61e52598798
Simone Martelli <simone.martelli@emea.nttdata.com>
2022-11-11 17:01:20 +0100

    added total count API get api-keys, bug fix

--- src/main/java/it/pagopa/pn/apikey/manager/constant/AggregationConstant.java
+++ src/main/java/it/pagopa/pn/apikey/manager/constant/AggregationConstant.java
@@ -8,5 +8,6 @@ public final class AggregationConstant {
     public static final String PK = "aggregateId";
     public static final String NAME = "aggregateName"; // TODO cambiare in name
     public static final String GSI_NAME = "aggregations-aggregateName-index";
-    public static final String PAGEABLE = "Y";
+    public static final String PAGEABLE = "pageable";
+    public static final String PAGEABLE_VALUE = "Y";
 }
