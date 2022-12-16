8d8f35251c2bdb0582c153004013cc1c690515a8
Simone Martelli <simone.martelli@emea.nttdata.com>
2022-12-16 12:21:52 +0100

    fix apikey pagination with groups

--- src/main/java/it/pagopa/pn/apikey/manager/repository/ApiKeyRepositoryImpl.java
+++ src/main/java/it/pagopa/pn/apikey/manager/repository/ApiKeyRepositoryImpl.java
@@ -3,6 +3,7 @@ package it.pagopa.pn.apikey.manager.repository;
 import it.pagopa.pn.apikey.manager.constant.ApiKeyConstant;
 import it.pagopa.pn.apikey.manager.entity.ApiKeyModel;
 import it.pagopa.pn.apikey.manager.exception.ApiKeyManagerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,8 @@ import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
 import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,11 +24,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import static it.pagopa.pn.apikey.manager.exception.ApiKeyManagerExceptionError.APIKEY_DOES_NOT_EXISTS;
 
+@Slf4j
 @Component
 public class ApiKeyRepositoryImpl implements ApiKeyRepository {
 
     private final DynamoDbAsyncTable<ApiKeyModel> table;
     private final String gsiLastUpdate;
+
+    private static final int MIN_LIMIT = 100;
 
     public ApiKeyRepositoryImpl(DynamoDbEnhancedAsyncClient dynamoDbEnhancedClient,
                                 @Value("${pn.apikey.manager.dynamodb.apikey.gsi-name.pa-id}") String gsiLastUpdate,
@@ -57,6 +63,13 @@ public class ApiKeyRepositoryImpl implements ApiKeyRepository {
 
     @Override
     public Mono<Page<ApiKeyModel>> getAllWithFilter(String xPagopaPnCxId, List<String> xPagopaPnCxGroups, ApiKeyPageable pageable) {
+        return getAllWithFilter(xPagopaPnCxId, xPagopaPnCxGroups, new ArrayList<>(), pageable);
+    }
+
+    private Mono<Page<ApiKeyModel>> getAllWithFilter(String xPagopaPnCxId,
+                                                     List<String> xPagopaPnCxGroups,
+                                                     List<ApiKeyModel> cumulativeQueryResult,
+                                                     ApiKeyPageable pageable) {
         Map<String, AttributeValue> expressionValues = new HashMap<>();
 
         Expression expression = Expression.builder()
@@ -76,21 +89,69 @@ public class ApiKeyRepositoryImpl implements ApiKeyRepository {
                 .keyEqualTo(Key.builder().partitionValue(xPagopaPnCxId)
                         .build());
 
+        Integer limit = pageable.getLimit();
+        if (limit != null && limit < MIN_LIMIT) {
+            limit = MIN_LIMIT;
+        }
+        log.debug("limit from pageable {} - actual limit {}", pageable.getLimit(), limit);
         QueryEnhancedRequest queryEnhancedRequest = QueryEnhancedRequest.builder()
                 .queryConditional(queryConditional)
                 .exclusiveStartKey(startKey)
                 .filterExpression(expression)
                 .scanIndexForward(false)
-                .limit(pageable.getLimit())
+                .limit(limit)
                 .build();
 
         if (pageable.hasLimit()) {
-            return Mono.from(table.index(gsiLastUpdate).query(queryEnhancedRequest));
+            return Mono.from(table.index(gsiLastUpdate).query(queryEnhancedRequest))
+                    .flatMap(page -> {
+                        cumulativeQueryResult.addAll(page.items());
+                        Map<String, AttributeValue> lastEvaluatedKey = null;
+                        if (page.lastEvaluatedKey() != null) {
+                            lastEvaluatedKey = new HashMap<>(page.lastEvaluatedKey());
+                        }
+                        if (cumulativeQueryResult.size() <= pageable.getLimit() && page.lastEvaluatedKey() != null) {
+                            ApiKeyPageable newPageable = getNewPageable(page, pageable);
+                            log.trace("get new page with pageable {}", newPageable);
+                            return getAllWithFilter(xPagopaPnCxId, xPagopaPnCxGroups, cumulativeQueryResult, newPageable);
+                        }
+                        List<ApiKeyModel> result = adjustPageResult(cumulativeQueryResult, pageable, lastEvaluatedKey);
+                        return Mono.just(Page.create(result, lastEvaluatedKey));
+                    });
         } else {
             return Flux.from(table.index(gsiLastUpdate).query(queryEnhancedRequest).flatMapIterable(Page::items))
                     .collectList()
                     .map(Page::create);
         }
+    }
+
+    private ApiKeyPageable getNewPageable(Page<ApiKeyModel> page, ApiKeyPageable pageable) {
+        return ApiKeyPageable.builder()
+                .lastEvaluatedKey(page.lastEvaluatedKey().get(ApiKeyConstant.PK).s())
+                .lastEvaluatedLastUpdate(page.lastEvaluatedKey().get(ApiKeyConstant.LAST_UPDATE).s())
+                .limit(pageable.getLimit())
+                .build();
+    }
+
+    private List<ApiKeyModel> adjustPageResult(List<ApiKeyModel> result,
+                                               ApiKeyPageable pageable,
+                                               Map<String, AttributeValue> lastEvaluatedKey) {
+        if (pageable.hasLimit() && result.size() > pageable.getLimit()) {
+            log.debug("need to truncate last page - size from {} to {}", result.size(), pageable.getLimit());
+            result = result.subList(0, pageable.getLimit());
+            if (!result.isEmpty() && lastEvaluatedKey != null) {
+                log.debug("need to adjust last evaluated key from {}", lastEvaluatedKey);
+                ApiKeyModel lastElement = result.get(result.size() - 1);
+                log.debug("last element is {}", lastElement);
+                lastEvaluatedKey.put(ApiKeyConstant.PK, AttributeValue.builder().s(lastElement.getId()).build());
+                if (lastElement.getLastUpdate() != null) {
+                    lastEvaluatedKey.put(ApiKeyConstant.LAST_UPDATE, AttributeValue.builder().s(lastElement.getLastUpdate()
+                            .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)).build());
+                }
+                log.debug("new last evaluated key is {}", lastEvaluatedKey);
+            }
+        }
+        return result;
     }
 
     @Override
