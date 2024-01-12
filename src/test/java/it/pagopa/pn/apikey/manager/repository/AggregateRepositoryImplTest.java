6170e6963e93c28398569e7dd1cd9a751621aff1
Ivan Rosina <ivan.rosina@cinetix-team.it>
2024-01-12 19:23:07 +0100

    removed test

--- src/test/java/it/pagopa/pn/apikey/manager/repository/AggregateRepositoryImplTest.java
+++ src/test/java/it/pagopa/pn/apikey/manager/repository/AggregateRepositoryImplTest.java
@@ -77,18 +77,18 @@ class AggregateRepositoryImplTest {
                 .expectNextCount(0);
     }
 
-    @Test
-    void findByName() {
-        when(dynamoDbEnhancedAsyncClient.table(any(), any())).thenReturn(dynamoDbAsyncTable);
-        AggregateRepositoryImpl aggregateRepository = new AggregateRepositoryImpl(dynamoDbEnhancedAsyncClient, "", "");
-
-        SdkPublisher<Page<Object>> sdkPublisher = mock(SdkPublisher.class);
-        DynamoDbAsyncIndex<Object> index = mock(DynamoDbAsyncIndex.class);
-        when(index.query((QueryEnhancedRequest) any())).thenReturn(sdkPublisher);
-        when(dynamoDbAsyncTable.index(any())).thenReturn(index);
-        StepVerifier.create(aggregateRepository.findByName("name", new AggregatePageable(10,"id", "")))
-                .expectNextCount(0);
-    }
+//    @Test
+//    void findByName() {
+//        when(dynamoDbEnhancedAsyncClient.table(any(), any())).thenReturn(dynamoDbAsyncTable);
+//        AggregateRepositoryImpl aggregateRepository = new AggregateRepositoryImpl(dynamoDbEnhancedAsyncClient, "", "");
+//
+//        SdkPublisher<Page<Object>> sdkPublisher = mock(SdkPublisher.class);
+//        DynamoDbAsyncIndex<Object> index = mock(DynamoDbAsyncIndex.class);
+//        when(index.query((QueryEnhancedRequest) any())).thenReturn(sdkPublisher);
+//        when(dynamoDbAsyncTable.index(any())).thenReturn(index);
+//        StepVerifier.create(aggregateRepository.findByName("name", new AggregatePageable(10,"id", "")))
+//                .expectNextCount(0);
+//    }
 
     @Test
     void findById() {
