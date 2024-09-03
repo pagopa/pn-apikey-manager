22f4852a0e9ba0ea767434169fe3c04d9e8c1c5a
Stefano Rughetti <stefano.rughetti@emeal.nttdata.com>
2024-09-03 16:36:29 +0200

    [PN-12356] Feat: Added index for uid and cxId

--- src/main/java/it/pagopa/pn/apikey/manager/constant/ApiKeyConstant.java
+++ src/main/java/it/pagopa/pn/apikey/manager/constant/ApiKeyConstant.java
@@ -13,8 +13,10 @@ public final class ApiKeyConstant {
     public static final String GROUPS = "groups";
     public static final String PA_ID = "x-pagopa-pn-cx-id";
     public static final String VIRTUAL_KEY = "virtualKey";
+    public static final String UID = "x-pagopa-pn-uid";
     public static final String GSI_VK = "virtualKey-id-index";
     public static final String GSI_PA = "paId-lastUpdate-index";
+    public static final String GSI_UID_CXID = "uid-cxId-index";
 
     public static final Set<CxTypeAuthFleetDto> ALLOWED_CX_TYPE = Set.of(CxTypeAuthFleetDto.PA);
 }
