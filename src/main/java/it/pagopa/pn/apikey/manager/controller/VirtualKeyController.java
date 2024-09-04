7ddac3eef93c1a9e0c1205d4083ada043c8d1d88
federica.moschese <federica.moschese@emeal.nttdata.com>
2024-09-04 15:34:27 +0200

    [PN-12388] managed api to delete virtual key

--- src/main/java/it/pagopa/pn/apikey/manager/controller/VirtualKeyController.java
+++ src/main/java/it/pagopa/pn/apikey/manager/controller/VirtualKeyController.java
@@ -1,4 +1,67 @@
 package it.pagopa.pn.apikey.manager.controller;
 
-public class VirtualKeyController {
+import it.pagopa.pn.apikey.manager.generated.openapi.server.v1.api.VirtualKeysApi;
+import it.pagopa.pn.apikey.manager.generated.openapi.server.v1.dto.CxTypeAuthFleetDto;
+import it.pagopa.pn.apikey.manager.service.VirtualKeyService;
+import it.pagopa.pn.apikey.manager.utils.CheckExceptionUtils;
+import it.pagopa.pn.commons.log.PnAuditLogBuilder;
+import it.pagopa.pn.commons.log.PnAuditLogEvent;
+import it.pagopa.pn.commons.log.PnAuditLogEventType;
+import lombok.AllArgsConstructor;
+import lombok.CustomLog;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@CustomLog
+@AllArgsConstructor
+public class VirtualKeyController implements VirtualKeysApi {
+
+    private final VirtualKeyService virtualKeyService;
+    private final PnAuditLogBuilder auditLogBuilder;
+
+    /**
+     * DELETE /virtual-keys/{id} : Eliminazione virtual key
+     * Servizio di eliminazione di una virtual key.
+     *
+     * @param xPagopaPnUid User Identifier (required)
+     * @param xPagopaPnCxType Customer/Receiver Type (required)
+     * @param xPagopaPnCxId Customer/Receiver Identifier (required)
+     * @param xPagopaPnCxRole User role (required)
+     * @param id Identificativo univoco della virtual key. (required)
+     * @param xPagopaPnCxGroups Customer Groups (optional)
+     * @return OK (status code 200)
+     *         or Bad request (status code 400)
+     *         or Wrong state transition (status code 409)
+     *         or Not found (status code 404)
+     *         or Internal error (status code 500)
+     */
+    @Override
+    public Mono<ResponseEntity<Void>> deleteVirtualKey(String xPagopaPnUid, CxTypeAuthFleetDto xPagopaPnCxType,
+                                                       String xPagopaPnCxId, String xPagopaPnCxRole,
+                                                       String id, List<String> xPagopaPnCxGroups, final ServerWebExchange exchange) {
+        String logMessage = String.format("Cancellazione Virtual Key - xPagopaPnUid=%s - xPagopaPnCxType=%s - xPagopaPnCxId=%s - xPagopaPnCxRole=%s - id=%s",
+                xPagopaPnUid,
+                xPagopaPnCxType,
+                xPagopaPnCxId,
+                xPagopaPnCxRole,
+                id);
+
+        PnAuditLogEvent logEvent = auditLogBuilder
+                .before(PnAuditLogEventType.AUD_AK_DELETE, logMessage)
+                .build();
+
+        logEvent.log();
+
+        return virtualKeyService.deleteVirtualKey(id,xPagopaPnUid, xPagopaPnCxType, xPagopaPnCxId, xPagopaPnCxGroups, xPagopaPnCxRole)
+                .doOnError(throwable -> CheckExceptionUtils.logAuditOnErrorOrWarnLevel(throwable, logEvent))
+                .map(s -> {
+                    logEvent.generateSuccess(logMessage).log();
+                    return ResponseEntity.ok().build();
+                });
+    }
 }
--- src/main/java/it/pagopa/pn/apikey/manager/service/VirtualKeyService.java
+++ src/main/java/it/pagopa/pn/apikey/manager/service/VirtualKeyService.java
@@ -1,4 +1,89 @@
 package it.pagopa.pn.apikey.manager.service;
 
+import it.pagopa.pn.apikey.manager.constant.VirtualKeyConstant;
+import it.pagopa.pn.apikey.manager.entity.ApiKeyHistoryModel;
+import it.pagopa.pn.apikey.manager.entity.ApiKeyModel;
+import it.pagopa.pn.apikey.manager.exception.ApiKeyManagerException;
+import it.pagopa.pn.apikey.manager.generated.openapi.server.v1.dto.CxTypeAuthFleetDto;
+import it.pagopa.pn.apikey.manager.generated.openapi.server.v1.dto.VirtualKeyStatusDto;
+import it.pagopa.pn.apikey.manager.repository.ApiKeyRepository;
+import it.pagopa.pn.apikey.manager.utils.VirtualKeyUtils;
+import lombok.AllArgsConstructor;
+import lombok.CustomLog;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static it.pagopa.pn.apikey.manager.exception.ApiKeyManagerExceptionError.APIKEY_CAN_NOT_DELETE;
+import static it.pagopa.pn.apikey.manager.exception.ApiKeyManagerExceptionError.APIKEY_CX_TYPE_NOT_ALLOWED;
+
+@Service
+@AllArgsConstructor
+@CustomLog
 public class VirtualKeyService {
+    private final ApiKeyRepository apiKeyRepository;
+
+    public Mono<String> deleteVirtualKey(String id, String xPagopaPnUid, CxTypeAuthFleetDto xPagopaPnCxType, String xPagopaPnCxId, List<String> xPagopaPnCxGroups, String xPagopaPnCxRole) {
+
+        if (!VirtualKeyConstant.ALLOWED_CX_TYPE_VIRTUAL_KEY.contains(xPagopaPnCxType)) {
+            log.error("CxTypeAuthFleet {} not allowed", xPagopaPnCxType);
+            return Mono.error(new ApiKeyManagerException(String.format(APIKEY_CX_TYPE_NOT_ALLOWED, xPagopaPnCxType), HttpStatus.FORBIDDEN));
+        }
+
+        return apiKeyRepository.findById(id)
+                .flatMap(virtualKeyModel -> this.validateRole(virtualKeyModel, xPagopaPnUid, xPagopaPnCxRole, xPagopaPnCxGroups))
+                .flatMap(this::isOperationAllowed)
+                .flatMap(virtualKeyModel -> this.createVirtualKeyModelToDelete(virtualKeyModel, xPagopaPnUid, xPagopaPnCxType, xPagopaPnCxId, xPagopaPnCxGroups, id))
+                .flatMap(apiKeyRepository::save)
+                .thenReturn("VirtualKey deleted");
+    }
+
+    private Mono<ApiKeyModel> createVirtualKeyModelToDelete(ApiKeyModel virtualKeyModel, String xPagopaPnUid, CxTypeAuthFleetDto xPagopaPnCxType, String xPagopaPnCxId, List<String> xPagopaPnCxGroups, String id) {
+
+        ApiKeyModel apiKeyModel = new ApiKeyModel();
+        apiKeyModel.setId(id);
+        apiKeyModel.setVirtualKey(id);
+        apiKeyModel.setStatus(virtualKeyModel.getStatus());
+        apiKeyModel.setCxId(xPagopaPnCxId);
+        apiKeyModel.setCxGroup(xPagopaPnCxGroups);
+        apiKeyModel.setName(virtualKeyModel.getName());
+        apiKeyModel.setPdnd(virtualKeyModel.isPdnd());
+        apiKeyModel.setUid(xPagopaPnUid);
+        apiKeyModel.setCxType(xPagopaPnCxType.toString());
+
+        ApiKeyHistoryModel apiKeyHistoryModel = new ApiKeyHistoryModel();
+        //TODO aggiungere un nuovo stato nel VirtualKeyStatusDto ?
+        apiKeyHistoryModel.setStatus("DELETED");
+        apiKeyHistoryModel.setDate(LocalDateTime.now());
+        apiKeyHistoryModel.setChangeByDenomination(xPagopaPnUid);
+
+        List<ApiKeyHistoryModel> apiKeyHistoryModelList = new ArrayList<>(apiKeyModel.getStatusHistory());
+        apiKeyHistoryModelList.add(apiKeyHistoryModel);
+
+        apiKeyModel.setStatusHistory(apiKeyHistoryModelList);
+
+        return Mono.just(apiKeyModel);
+    }
+
+    private Mono<ApiKeyModel> validateRole(ApiKeyModel virtualKeyModel, String xPagopaPnUid, String xPagopaPnCxRole, List<String> xPagopaPnCxGroups) {
+        return VirtualKeyUtils.isRoleAdmin(xPagopaPnCxRole, xPagopaPnCxGroups)
+                .flatMap(isAdmin -> {
+                    if (!isAdmin && !virtualKeyModel.getUid().equals(xPagopaPnUid)) {
+                        return Mono.error(new ApiKeyManagerException(String.format(APIKEY_CAN_NOT_DELETE, virtualKeyModel.getStatus()), HttpStatus.BAD_REQUEST));
+                    }
+                    return Mono.just(virtualKeyModel);
+                });
+    }
+
+    private Mono<ApiKeyModel> isOperationAllowed(ApiKeyModel virtualKeyModel) {
+        VirtualKeyStatusDto status = VirtualKeyStatusDto.fromValue(virtualKeyModel.getStatus());
+        if (!status.getValue().equals(VirtualKeyStatusDto.BLOCKED.getValue())) {
+            return Mono.error(new ApiKeyManagerException(String.format(APIKEY_CAN_NOT_DELETE, virtualKeyModel.getStatus()), HttpStatus.BAD_REQUEST));
+        }
+        return Mono.just(virtualKeyModel);
+    }
 }
