8733f3364524e7dce3fe80af8a0731ced4ca256f
DacunzoMa <matteo.dacunzo@emeal.nttdata.com>
2024-10-29 17:25:36 +0100

    PN-10355 Feature: Added SpringAnalyzer config

--- src/main/java/it/pagopa/pn/apikey/manager/ApiKeyManagerApplication.java
+++ src/main/java/it/pagopa/pn/apikey/manager/ApiKeyManagerApplication.java
@@ -1,13 +1,18 @@
 package it.pagopa.pn.apikey.manager;
 
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiKeyManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiKeyManagerApplication.class, args);
+        SpringApplication app = new SpringApplication(ApiKeyManagerApplication.class);
+        app.addListeners(new TaskIdApplicationListener());
+        app.run(args);
     }
 
 }
